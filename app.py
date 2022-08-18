d7b9b105a4cbfbeee13baaad4e901fd86d54127e
guyman575 <rgcassetta@gmail.com>
2022-08-17 17:04:13 -0700

    adding travel info page and removing rsvp section. Moved navbar to common template

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app._static_folder = "static"
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/travel")
+def travel():
+    return render_template("travel.html")
