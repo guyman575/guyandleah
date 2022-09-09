e1d70f8be2f5c7201fc6a94884e5ad51286a145a
guyman575 <rgcassetta@gmail.com>
2022-09-09 11:00:03 -0700

    enforce ssl

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_mobility import Mobility
 
 app = Flask(__name__)
 app._static_folder = "static"
 Mobility(app)
+
+@app.before_request
+def before_request():
+    if app.debug or \
+        request.is_secure or \
+        request.url_root.startswith('http://127.0.0.1') or \
+        request.url_root.startswith('http://localhost'):
+        return
+    url = request.url.replace('http://', 'https://', 1)
+    code = 301
+    return redirect(url, code=code)
 
 @app.route("/")
 def home():
@@ -22,4 +33,4 @@ def faq():
     return render_template("faq.html")
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=9874)
+    app.run(host='localhost', port=8000)
