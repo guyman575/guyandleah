76f8f1acd31e69a3360b3e2dbed8ef338265ff4c
Leah Schatz <leahcschatz@gmail.com>
2022-09-30 10:44:16 -0700

    side bar width a little wider

--- static/js/newnav.js
+++ static/js/newnav.js
@@ -1,6 +1,6 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "190px";
-  document.getElementById("main").style.marginLeft = "190px";
+  document.getElementById("mySidebar").style.width = "220px";
+  document.getElementById("main").style.marginLeft = "220px";
 }
 
 function closeNav() {
