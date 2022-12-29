34526a14072b460096f5d9be854c418f0b27a960
guyman575 <rgcassetta@gmail.com>
2022-12-29 12:45:22 -0800

    adding rsvp functionality via google sheets backed db

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, session
 from flask_mobility import Mobility
 import flask
+import gspread
+import json
+from oauth2client.service_account import ServiceAccountCredentials
+import re
+import os
+
 
 app = Flask(__name__)
 app._static_folder = "static"
 app.secret_key = 'butts'
 Mobility(app)
+credentials_dict = json.loads(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
+credential = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict,
+    ["https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"])
+client = gspread.authorize(credential)
 
 @app.before_request
 def before_request():
@@ -40,19 +53,68 @@ def registry():
 
 @app.route("/rsvp", methods=['GET', 'POST'])
 def rsvp():
+    session['not_found'] = False
     if session.get('reservation'):
         session.pop('reservation')
+
+    if flask.request.method == 'POST':
+        rsvps = []
+        for i in range(2):
+            if request.form.get(f"attending_{i}"):
+                rsvps.append(Rsvp.from_form(request.form,i))
+        makeRsvp(rsvps)
     return render_template("rsvp.html")
 
 @app.route("/reservation", methods=['POST'])
 def reservation():
-
-    reservation = {'attendees': [
-        {'name': 'John Doe', 'rehearsal': True},
-        {'name': 'Jane Doe', 'rehearsal': False}
-    ]}
-    session['reservation'] = reservation
+    # Sanitize input so dumb friends cant code inject us
+    input_name = re.sub(r'[^a-zA-Z\s]', '',request.form.get('name'))
+    print(input_name)
+    reservations = getReservationData(input_name)
+    print(reservations)
+    if reservations:
+        session['reservation'] = reservations
+        session['not_found'] = False
+    else:
+        session['not_found'] = True
     return render_template("rsvp.html")
+
+def getReservationData(name):
+    reservations_db_sheet = client.open_by_key('14Je37BKWxVVIXHfiMnGu37m_0KI9pKyept4ZOVotWW8').worksheet("reservations")
+    all_reservations = reservations_db_sheet.get_all_records()
+    print(all_reservations)
+    invite_id = 0
+    for reservation in all_reservations:
+        if reservation['name'].lower() == name.lower():
+            invite_id = reservation['invite_id']
+    
+    matching_reservations = [res for res in all_reservations if res['invite_id'] == invite_id]
+    return matching_reservations
+
+class Rsvp:
+    def __init__(self,name,invite_id,attending,rehearsal_attending, food):
+        self.name = name
+        self.invite_id = invite_id
+        self.attending = attending
+        self.rehearsal_attending = rehearsal_attending
+        self.food = food
+
+    @classmethod
+    def from_form(cls,form,index):
+        name = form.get(f"rsvp_name_{index}")
+        id = form.get(f"rsvp_id_{index}")
+        attending = form.get(f"attending_{index}")
+        rehearsal = form.get(f"rehearsal_{index}")
+        food = form.get(f"food_pref_{index}")
+        return Rsvp(name,id,attending,rehearsal,food)
+
+        
+
+def makeRsvp(rsvps):
+    rsvp_sheet = client.open_by_key('14Je37BKWxVVIXHfiMnGu37m_0KI9pKyept4ZOVotWW8').worksheet("RSVPs")
+    values = [[rsvp.name, rsvp.invite_id, rsvp.attending, rsvp.rehearsal_attending, rsvp.food] for rsvp in rsvps]
+    rsvp_sheet.append_rows(values,insert_data_option='INSERT_ROWS')
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8000)
