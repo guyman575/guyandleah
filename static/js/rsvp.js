2caef35be343448fbe6b89e920489fd3ffff2520
guyman575 <rgcassetta@gmail.com>
2023-04-09 15:08:12 -0700

    fixing same bug for rehearsal wasnt as hard as i thought

--- static/js/rsvp.js
+++ static/js/rsvp.js
@@ -9,10 +9,16 @@ for (let i = 0; i < MAX_GROUP_SIZE; i++){
                 $(`input[name$="food_pref_${i}"]`).each(function() {
                     $(this).prop('required', true);
                 });
+                $(`input[name$="rehearsal_${i}"]`).each(function() {
+                    $(this).prop('required', true);
+                });
             }
             else {
                 $(`#ifattending_${i}`).hide();
                 $(`input[name$="food_pref_${i}"]`).each(function() {
+                    $(this).prop('required', false);
+                });
+                $(`input[name$="rehearsal_${i}"]`).each(function() {
                     $(this).prop('required', false);
                 });
             }
